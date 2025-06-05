e4c65deae039fb1c25515b7676ff17d14bbd4d95
Pramuditha <pramudithakavinda01@gmail.com>
2025-06-05 23:59:15 +0530

    Add Employee function created

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import EmployeeComponent from "./components/EmployeeComponent";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<ListEmployeeComponent />}></Route>
           {/* http://localhost:3000/employees */}
           <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+          {/* http://localhost:3000/add-employee */}
+          <Route path="/add-employee" element={<EmployeeComponent />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
