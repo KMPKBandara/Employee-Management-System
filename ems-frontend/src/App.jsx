320ac289f04b6ac921c4a6df7bd063b8ed25a4d1
Pramuditha <pramudithakavinda01@gmail.com>
2025-06-06 14:51:50 +0530

    Completed Styling

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import EmployeeComponent from "./components/EmployeeComponent";
 import Footer from "./components/Footer";
--- ems-frontend/src/components/Footer.jsx
+++ ems-frontend/src/components/Footer.jsx
@@ -1,16 +1,3 @@
-/*import React from "react";
-
-const Footer = () => {
-  return (
-    <div>
-      <footer className="footer">
-        <span>All rights reserved 2025 by pramuditha </span>
-      </footer>
-    </div>
-  );
-};
-
-export default Footer;*/
 import React from "react";
 import { FaRegCopyright } from "react-icons/fa";
 import "./css/Footer.css";
@@ -20,22 +7,25 @@ const Footer = () => {
     <footer className="simple-footer bg-light border-top mt-auto">
       <div className="container py-4">
         <div className="row align-items-center">
-          {/* Center - Description */}
-          <div className="col-md-4 text-center mb-3 mb-md-0">
+          {/* Left Side - Description */}
+          <div className="col-md-4 text-center text-md-start mb-3 mb-md-0">
             <p className="footer-description mb-0">
               Employee Management System
             </p>
           </div>
 
-          {/* Right Side - Copyright */}
-          <div className="col-md-4 text-center text-md-end">
-            <div className="d-flex align-items-center justify-content-center justify-content-md-end">
+          {/* Center - Copyright */}
+          <div className="col-md-4 text-center mb-3 mb-md-0">
+            <div className="d-flex align-items-center justify-content-center">
               <FaRegCopyright className="me-1" />
               <span className="copyright-text">
                 2025 <strong>EMS</strong>. All rights reserved.
               </span>
             </div>
           </div>
+
+          {/* Right Side - Empty column to balance the layout */}
+          <div className="col-md-4"></div>
         </div>
       </div>
     </footer>
